022f675628335f7695fb016ac9070d7d23c84c5b
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-10-29 22:23:06 +0000

    initial solution, not tested

--- mergeTwoLinkedList.py
+++ mergeTwoLinkedList.py
@@ -4,6 +4,28 @@
 #     self.value = x
 #     self.next = None
 #
+def mergeTwoLinkedLists(l1, l2):
 
-def mergeTwoLinkedList:
+    # edgecase: no valid list
+    if(l1==None and l2==None): return []
     
+    # edge case: only one valid list
+    if(l1==None and l2!=None): return l2
+    if(l2==None and l1!=None): return l1
+
+    
+    mergedList = []
+    
+    while(l1):
+        
+        if(l1.value < l2.value):
+            mergedList.append(l1.value)
+            print(mergedList)
+            l1 = l1.next
+        else:
+            mergedList.append(l2.value)
+            print(mergedList)
+            l2 = l2.next
+    
+            
+    return mergedList
