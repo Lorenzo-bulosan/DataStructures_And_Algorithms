a1196f39657ea7b67f414286a9c9b8837e6a6261
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-10-30 22:02:06 +0000

    manual testing added, need to add unit testing

--- mergeTwoLinkedList.py
+++ mergeTwoLinkedList.py
@@ -42,3 +42,46 @@ def mergeTwoLinkedLists(l1, l2):
                 l2 = l2.next
       
     return mergedList
+
+#%% Create link list to test
+class ListNode(object):
+    def __init__(self, x):
+        self.value = x
+        self.next = None
+
+# populate first list nodes
+x = ListNode(1)
+x.next = ListNode(2)
+x.next.next = ListNode(3)
+x.next.next.next = ListNode(4)
+x.next.next.next.next = ListNode(1)
+
+# visualise first list
+firstList = []
+temp = x
+while(temp):
+    firstList.append(temp.value)
+    temp = temp.next
+print('First list:')
+print(firstList)
+
+# populate second list
+y = ListNode(2)
+y.next = ListNode(2)
+y.next.next = ListNode(7)
+y.next.next.next = ListNode(8)
+y.next.next.next.next = ListNode(10)
+
+# visualise first list
+secondList = []
+temp = y
+while(temp):
+    secondList.append(temp.value)
+    temp = temp.next
+print('Second list:')
+print(secondList)
+
+#%% Test function
+out = mergeTwoLinkedLists(x,y)
+print('Resulting list:')
+print(out)
