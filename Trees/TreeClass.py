1a8cfc7d54489924ac96c13c506944a1466f1e08
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-03 10:44:10 +0000

    finished method for insertion to the binary tree

--- Trees/TreeClass.py
+++ Trees/TreeClass.py
@@ -7,8 +7,71 @@ class Node():
         self.right = None
         self.left = None
 
-class Tree():
+class BinarySearchTree():
     def __init__(self):
         self.root = None
-        self.length = 0
         
+    def recursiveInsert(self,current, a):
+        
+        print('Comparing', a,'to', current.value)
+        
+        if(a>current.value):
+            #check if there's already a node
+            print('inserting to the right')
+            if(current.right==None):
+                print('no right node, inserting now')
+                current.right = Node(a)
+                return self
+            else:
+                print('already existing node, repeat')
+                current = current.right
+                self.recursiveInsert(current,a)
+        else:
+            print('inserting to the left')
+            if(current.left==None):
+                print('no left node, inserting now')
+                current.left = Node(a)
+                return self
+            else:
+                print('already existing node, repeat')
+                current = current.left
+                self.recursiveInsert(current,a)
+    
+    def insert(self,a):
+        
+        #check if empty tree
+        if(self.root==None):
+            self.root = Node(a)
+            print('empty tree, creating node:', ' ' ,a)
+            return self
+                    
+        #decide to look left/right and check if theres a node 
+        current = self.root
+        self.recursiveInsert(current,a)
+                
+            
+        
+
+#%% Basic test
+tree = BinarySearchTree()
+tree.root = Node(10)
+tree.root.right = Node(15)
+tree.root.left = Node(5)
+tree.root.left.left = Node(0)
+tree.root.left.right = Node(8)
+
+#%% Testing insert method
+tree = BinarySearchTree()
+
+# edge case: empty tree
+a = 10
+print('inserting: ', a)
+out = tree.insert(a)
+print('\n')
+
+# test inserting to existing tree
+numToInsert = [7, 15, 0, 20]
+for i in numToInsert:
+    print('inserting: ', i)
+    out.insert(i)
+    print('\n')
