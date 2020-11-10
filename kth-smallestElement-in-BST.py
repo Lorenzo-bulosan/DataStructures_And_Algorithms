322a4a5b59785b8c846d3dae29945d94ee3ec218
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-10 22:44:54 +0000

    creating unit testing, test 1

--- kth-smallestElement-in-BST.py
+++ kth-smallestElement-in-BST.py
@@ -20,3 +20,28 @@ def kthSmallestInBST(t, k):
     DFSinOrder(t)
     
     return listInOrder[k-1]
+
+#%% create binary tree for testing
+print('Conducting Test 1')
+print('creating test tree')
+from Trees import *
+tree1 = BinarySearchTree()
+numToInsert = [10,5,15,0,8,12,20] 
+
+#populate binary tree
+for i in numToInsert:
+    tree1.insert(i)
+    
+#unit test 1--------------------------------
+print('\n')
+correct = 15
+pos = 6
+t = tree1.root
+
+if(kthSmallestInBST(t,pos)==correct):
+    print('Test 1 Passed')
+else:
+    print('The wrong answer:',kthSmallestInBST(t,pos))
+    print('Failed to identify correct answer')
+
+print('Test 1 finished\n')
