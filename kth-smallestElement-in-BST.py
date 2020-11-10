7f00cba414c16f892d427cf7ae3f8dc158d869f2
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-10 22:36:48 +0000

    solution to kth smallest element in BST

--- kth-smallestElement-in-BST.py
+++ kth-smallestElement-in-BST.py
@@ -1,1 +1,22 @@
+def kthSmallestInBST(t, k):
 
+    # recursive helper function
+    def DFSinOrder(t):
+        # check left nodes if they exist 
+        if(t.left):
+            DFSinOrder(t.left)
+            
+        # append to list
+        listInOrder.append(t.value)
+        
+        # check right nodes if they exist
+        if(t.right):
+            DFSinOrder(t.right)   
+        
+        return 
+    
+    # ordered values from the tree
+    listInOrder = []
+    DFSinOrder(t)
+    
+    return listInOrder[k-1]
