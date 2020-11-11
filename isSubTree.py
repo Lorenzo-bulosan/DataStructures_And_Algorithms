bfcda8efdf940cd993e6ee9eab169b365b4a8e65
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-11 13:13:34 +0000

    check point

--- isSubTree.py
+++ isSubTree.py
@@ -29,21 +29,23 @@ def isSubtree(t1, t2):
         
         print('cheking first tree:',tree1.value,tree2.value)
         
+        result = False
+        
         if(tree1.value==tree2.value):
             result = DFSpreOrderParallelCheck(tree1,tree2)
             return result
-
+    
         #check left
         print('checking left')
-        if(tree1.left!=None):
+        if(tree1.left!=None and result==False):
             DFSpreOrder(tree1.left,tree2)
         
         #check right
         print('checking right')
-        if(tree1.right!=None):
+        if(tree1.right!=None and result==False):
             DFSpreOrder(tree1.right,tree2)
-              
-        return False  
+         
+        return result  
         
     #edge case: t1 empty
     if(t1==None and t2!=None):
