be63cda8242546e3832aea3fecff71e78509fe15
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-14 15:06:05 +0000

    testing solution to isSubTree

--- isSubTree.py
+++ isSubTree.py
@@ -13,10 +13,21 @@ def isSubtree(t1, t2):
         
         # exit as soon as not the same
         if(tree1.value!=tree2.value): 
+            print('not same value, exiting')
             return False
         
         # if same value check the rest of the tree
         else:
+            
+            if(tree1.left==None and tree1.right==None and tree2.left==None and tree2.right==None):
+                print('reached leaf node')
+                if(tree1.value==tree2.value):
+                    print('same value')
+                    return True
+                else:
+                    print('leaf node but not same value')
+                    return False
+            
             if(tree1.left!=None and tree2.left!=None):
                 DFSpreOrderParallelCheck(tree1.left,tree2.left)
                         
@@ -33,17 +44,26 @@ def isSubtree(t1, t2):
         
         if(tree1.value==tree2.value):
             result = DFSpreOrderParallelCheck(tree1,tree2)
+            print('Found subree',result)
             return result
     
         #check left
         print('checking left')
         if(tree1.left!=None and result==False):
-            DFSpreOrder(tree1.left,tree2)
+            result = DFSpreOrder(tree1.left,tree2)
+#            if(result==True):
+#                return True
+#            else:
+#                return False
         
         #check right
         print('checking right')
         if(tree1.right!=None and result==False):
-            DFSpreOrder(tree1.right,tree2)
+            result = DFSpreOrder(tree1.right,tree2)
+#            if(result==True):
+#                return True
+#            else:
+#                return False
          
         return result  
         
@@ -61,3 +81,22 @@ def isSubtree(t1, t2):
     
     
     return DFSpreOrder(t1,t2)
+
+#%% create binary tree for testing
+print('creating test tree')
+from Trees import TreeClass
+
+#populate binary tree
+tree1 = BinarySearchTree()
+numToInsert1 = [10,5,15,0,8,12,20] 
+for i in numToInsert1:
+    tree1.insert(i)
+    
+#populate binary tree
+tree2 = BinarySearchTree()
+numToInsert2 = [5,842]
+for i in numToInsert2:
+    tree2.insert(i)
+
+#%% Testing function manually
+print(isSubtree(tree1.root,tree2.root))
