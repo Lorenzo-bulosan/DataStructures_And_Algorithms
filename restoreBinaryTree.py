dd52bd68b961ee064d20e3a6aaa5ea23002b13d0
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-17 21:07:41 +0000

    refactoring using recursive function

--- restoreBinaryTree.py
+++ restoreBinaryTree.py
@@ -6,51 +6,74 @@ class Tree(object):
      self.right = None
      
 def restoreBinaryTree(inorder, preorder):
+    ''' restores binary tree from 2 traversed list
     
-    # instantiate tree
-    tree = Tree(0)
-    inOrder = inorder
-    preOrder = preorder
- 
-    # find idx head in inOrder
-    head = 10
-    inOrder_head_idx = 0
-    
-    for i in inOrder:
-        if(i==head):
-            break
-        inOrder_head_idx += 1
+    In: int[] inOrder,
+        int[] preOrder
+    Out: TreeNode root node
+    '''
+    def helperRestore(startIdxPreOrder,startIdxInOrder,endIdxInOrder,inOrderList,preOrderList):
+        ''' recursive helperfunction that divides the list and sets head node
         
-    tree.value = head
-    print('preOrder head idx:',inOrder_head_idx)
-    
-    # divide nodes left/right from head as center
-    inStart = 0
-    inEnd = len(inOrder)
-    leftNodes = inOrder[inStart:inOrder_head_idx]
-    rightNodes = inOrder[len(leftNodes)+1:inEnd]
-    
-    print(preOrder)
-    print(leftNodes)
-    print(rightNodes)
-    
-    #find head from leftNodes by looking up preOrder
-    for i in leftNodes:
-        for j in preOrder:
-            if(i==j):
-                leftNewHead = j
+        In: int start idx of preOrderList, 
+            int start idx of inOrderList,
+            int end idx of inOrderList,
+            int[] preOrderList
+            int[] inOrderList
+        Out: TreeNode root node
+        '''
+        # check for boundaries on the indexs
+        if(startIdxPreOrder >= len(inOrderList)):
+            return None
+        
+        if(startIdxInOrder > endIdxInOrder):
+            return None
+        
+        # check empty lookup list
+        if(not inOrderList[startIdxInOrder:endIdxInOrder]):
+            return None
+     
+        # set head as the next in the list postOrder
+        head = preOrderList[startIdxPreOrder]
+        tree = Tree(head)
+        print('head is:',head)
+        
+        # find head index inside inOrderList within given range
+        print('looking inside:',[startIdxInOrder,endIdxInOrder],inOrderList[startIdxInOrder:endIdxInOrder])
+        headIdx = 0
+        for i in range(startIdxInOrder,endIdxInOrder):
+            if(inOrderList[i]==tree.value):
+                headIdx = i
+                break
+        print('head idx:',headIdx)
             
-    print('head in left:',leftNewHead)
+        # set left node: next head is next in preOrder, end of inOrder=current head idx
+        tree.left = helperRestore(startIdxPreOrder+1,startIdxInOrder,headIdx-1,\
+                                  inOrderList,preOrderList)
+        
+
+        # set left node, next head is the same (startIdxPreOrder+1) 
+        #   but also skipping all the left nodes = (headIdx-startIdxInOrder)
+        # the range of the list to look = head:end
+        print('right head starting at index',startIdxPreOrder+1+headIdx-startIdxInOrder)
+        tree.right = helperRestore(startIdxPreOrder+1+headIdx-startIdxInOrder,headIdx+1,endIdxInOrder,\
+                                 inOrderList,preOrderList)
+        
+        return tree
     
-    tree.left = leftNewHead
+    # initial values
+    startIdxPreOrder = 0
+    startIdxInOrder = 0
+    endIdxInOrder = len(inOrder)
+    inOrderList = inorder
+    preOrderList = preorder
     
-    #find head from rightNodes by looking up preOrder
-    for i in rightNodes:
-        for j in preOrder:
-            if(i==j):
-                rightNewHead = j
-            
-    print('head in right:',rightNewHead)
+    # visualize both list
+    print('inOrder:',inOrderList)
+    print('preOrder:',preOrderList)
+    
+    # call recursive helper function
+    tree = helperRestore(startIdxPreOrder,startIdxInOrder,endIdxInOrder,inOrderList,preOrderList)
     
     return tree
 
@@ -59,4 +82,9 @@ def restoreBinaryTree(inorder, preorder):
 inOrder = [0,5,10,7,15,20]
 preOrder = [10,5,0,7,15,20]
 
-restoreBinaryTree(inOrder, preOrder)
+tree = restoreBinaryTree(inOrder, preOrder)
+
+
+
+
+
