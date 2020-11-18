041b4be989a74b3644f83b592a4d67b325bc0832
Lorenzo Bulosan Bulosan <Lorenzo.bulosan@yahoo.co.uk>
2020-11-18 16:46:49 +0000

    method to validate input to graph

--- GraphClass.py
+++ GraphClass.py
@@ -9,11 +9,10 @@ class Graph():
     def addVertex(self, vertex: str) -> str:
         ''' method that adds a node/vertex
             In: str vertex
-            Out: adjacencyList
+            Out: str key added
         '''
         # input type validation
-        validInput = isinstance(vertex,str)
-        if(not validInput): return 'Invalid input'
+        if(not self.validateInput(vertex,str)): return 'Invalid Input'
         
         # check for duplicate
         try:
@@ -23,7 +22,24 @@ class Graph():
             self.adjacencyList[vertex]=[]
 
         return vertex
-            
+    
+    def addEdge_undirected(self, vertex1: str, vertex2: str) -> str:
+        ''' method that adds an edge/link between two vertices both ways
+        '''
+        return vertex1 + ' undirected conection to ' + vertex2
+        
+    def validateInput(self, inputToValidate, typeToValidate) -> bool:
+        ''' method that validates input to a given type
+            In: all inputToValidate 
+                all typeToValidate 
+            Out: bool validInput
+        '''
+        validInput = isinstance(inputToValidate,typeToValidate)
+        if(not validInput): return False
+        return True
+        
+        
+        
     
 #%% Testing methods
 testGraph = Graph()
